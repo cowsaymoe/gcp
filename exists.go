17cb7bedab6d4cd8c761a1bfc1b0e09a020ad11b
Mohammed <saleem@simplylion.com>
2022-07-05 23:44:28 -0400

    Check from file
    now will check from list.txt or user input file

--- exists.go
+++ exists.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func check_perms(url string) string {
@@ -22,20 +24,34 @@ func check_perms(url string) string {
 }
 
 func main() {
+	// wordlist to check
+	wordlist := []string{}
 	// flags
 	bucketFile := flag.String("file", "list.txt", "File of buckets to check")
 	flag.Parse()
 	fmt.Println("Checking buckets in", *bucketFile)
+	// open file
+	readFile, err := os.Open(*bucketFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// read file
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	// add each line to array of buckets
+	for fileScanner.Scan() {
+		wordlist = append(wordlist, fileScanner.Text())
+	}
+	readFile.Close()
 	// base url to crawl
 	base := ".storage.googleapis.com"
-	// wordlist to check
-	wordlist := []string{"sdfgsfgfsg", "abc", "discord", "fdsfgsdhgdgfhdgfhd"}
 
 	// function to check permissions of gcp storage bucket
 
 	for bucket := range wordlist {
 		// compile url for crawling
 		url := "https://" + wordlist[bucket] + base
+		fmt.Println("Checking ", wordlist[bucket])
 		resp, err := http.Get(url)
 		if err != nil {
 			log.Fatal(err)
